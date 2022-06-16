6d04ed00c701055fbf765f16d6b85ebd81cb1b51
Jeferson Almeida <jeferson.almeida@outlook.com>
2022-06-16 13:48:33 -0400

    Verticles

---
+++ src/main/java/com/jefersonalmeida/vertx_starter/verticles/MainVerticle.java
@@ -0,0 +1,21 @@
+package com.jefersonalmeida.vertx_starter.verticles;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+
+public class MainVerticle extends AbstractVerticle {
+
+  public static void main(String[] args) {
+    final var vertx = Vertx.vertx();
+    vertx.deployVerticle(new MainVerticle());
+  }
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    System.out.println("Start " + getClass().getName());
+    vertx.deployVerticle(new VerticleA());
+    vertx.deployVerticle(new VerticleB());
+    startPromise.complete();
+  }
+}
