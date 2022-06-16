4388e2bc405c7ad88c6694cd942d78c81d285cd8
Jeferson Almeida <jeferson.almeida@outlook.com>
2022-06-16 17:28:14 -0400

    Event Bus - Point to Point messaging

---
+++ src/main/java/com/jefersonalmeida/vertx_starter/eventbus/PointToPointExample.java
@@ -0,0 +1,45 @@
+package com.jefersonalmeida.vertx_starter.eventbus;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PointToPointExample {
+
+  private static final Logger LOG = LoggerFactory.getLogger(PointToPointExample.class);
+
+  public static void main(String[] args) {
+    final var vertx = Vertx.vertx();
+    vertx.deployVerticle(new Sender());
+    vertx.deployVerticle(new Receiver());
+  }
+
+  static class Sender extends AbstractVerticle {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Sender.class);
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+      startPromise.complete();
+      vertx.setPeriodic(1000, id -> {
+        // Send a message every second
+        vertx.eventBus().send(Sender.class.getName(), "Sending a message...");
+      });
+    }
+  }
+
+  static class Receiver extends AbstractVerticle {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Receiver.class);
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+      startPromise.complete();
+      vertx.eventBus().<String>consumer(Sender.class.getName(), message -> {
+        LOG.debug("Received: {}", message.body());
+      });
+    }
+  }
+}
