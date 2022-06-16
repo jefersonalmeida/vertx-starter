60f3dc6ec0d301e1202496466cef11b90400155e
Jeferson Almeida <jeferson.almeida@outlook.com>
2022-06-16 19:08:39 -0400

    Event Bus - Custom Message Codec

---
+++ src/main/java/com/jefersonalmeida/vertx_starter/eventbus/customcodec/Ping.java
@@ -0,0 +1,30 @@
+package com.jefersonalmeida.vertx_starter.eventbus.customcodec;
+
+public class Ping {
+  private String message;
+  private Boolean enabled;
+
+  public Ping() {
+  }
+
+  public Ping(String message, Boolean enabled) {
+    this.message = message;
+    this.enabled = enabled;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public Boolean isEnabled() {
+    return enabled;
+  }
+
+  @Override
+  public String toString() {
+    return "Ping{" +
+      "message='" + message + '\'' +
+      ", enabled=" + enabled +
+      '}';
+  }
+}
