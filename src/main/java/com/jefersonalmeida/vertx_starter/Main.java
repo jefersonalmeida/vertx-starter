3dc705443349a76b18d189c365ba114599d7dcd1
Jeferson Almeida <jeferson.almeida@outlook.com>
2022-06-16 20:55:08 -0400

    Live Redeploy

--- src/main/java/com/jefersonalmeida/vertx_starter/Main.java
+++ src/main/java/com/jefersonalmeida/vertx_starter/Main.java
@@ -6,6 +6,8 @@ import io.vertx.core.Vertx;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+
 public class Main extends AbstractVerticle {
 
   private static final Logger LOG = LoggerFactory.getLogger(Main.class);
@@ -17,18 +19,22 @@ public class Main extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req ->
+    vertx.createHttpServer()
+      .requestHandler(req ->
         req.response()
           .putHeader("content-type", "text/plain")
           .end("Hello from Vert.x!"))
       .listen(8888, http -> {
         if (http.succeeded()) {
           startPromise.complete();
-          LOG.debug("HTTP server started on port 8888");
-//          System.out.println("HTTP server started on port 8888");
+          LOG.info("HTTP server started on port 8888");
         } else {
           startPromise.fail(http.cause());
         }
       });
+
+//    vertx.setPeriodic(500, id -> {
+//      LOG.debug(String.valueOf(new Random().nextDouble()));
+//    });
   }
 }
